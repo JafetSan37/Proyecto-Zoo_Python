15bef9eb6623c7d1c37850fde034d5f0b71adc6c
unknown <andyduranmartinez@gmail.com>
2024-04-01 14:16:05 -0600

    guiones bajos care

--- Care.py
+++ Care.py
@@ -1,30 +1,30 @@
 class Care:
     def _init_(self, employee_in_charge, process_realized, id, process_date, observations):
-        self.employee_in_charge = employee_in_charge
-        self.process_realized = process_realized
-        self.id = id
-        self.process_date = process_date
-        self.observations = observations
+        self.__employee_in_charge = employee_in_charge
+        self.__process_realized = process_realized
+        self.__id = id
+        self.__process_date = process_date
+        self.__observations = observations
 
     def get_employee_in_charge(self):
-        return self.employee_in_charge
+        return self.__employee_in_charge
 
     def get_process_realized(self):
-        return self.process_realized
+        return self.__process_realized
 
     def get_id(self):
-        return self.id
+        return self.__id
 
     def get_process_date(self):
-        return self.process_date
+        return self.__process_date
 
     def get_observations(self):
-        return self.observations
+        return self.__observations
 
     def show_care(self):
-        print("Empleado a cargo: " + self.get_employee_in_charge().show_employee() +
-              "\nProceso: " + self.process_realized + "\n" +
-              "ID Animal: " + str(self.get_id()) +
-              "\nFecha Proceso: " + self.get_process_date().show_date() +
-              "\nObservaciones: " + self.get_observations())
+        print("Empleado a cargo: " + self.__get_employee_in_charge().show_employee() +
+              "\nProceso: " + self.__process_realized + "\n" +
+              "ID Animal: " + str(self.__get_id()) +
+              "\nFecha Proceso: " + self.__get_process_date().show_date() +
+              "\nObservaciones: " + self.__get_observations())
         print("......................................")
