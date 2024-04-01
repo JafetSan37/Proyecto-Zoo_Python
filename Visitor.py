986ade451467d5b29b62fcd7dcbe1a079be80831
JafetSan37 <j.s.b_95@outlook.com>
2024-03-31 23:11:13 -0600

    Traducción terminada

---
+++ Visitor.py
@@ -0,0 +1,48 @@
+from Person import Person
+from Date import Date
+class Visitor(Person):
+    def __init__(self, name, last_name, birth_date, curp,register_date):
+        super().__init__(name, last_name, birth_date, curp)
+        self.total_visits = 0
+        self.register_date = register_date
+    #Métodos de la clase
+    def get_total_visits(self):
+        return self.total_visits
+    def get_register_date(self,register_date):
+        return register_date
+    def set_total_visits(self,total_visits):
+        self.total_visits = total_visits
+    
+    def show_visitor(self):
+        return self.show_info() + f"Total visits: {self.get_total_visits} Register date: {self.get_register_date.show_date()}"
+    def modify(self):
+        print("\n¿Qué aspecto desea modificar?\n1) Nombre\n2) Apellido\n3) Fecha de Nacimiento\n4) CURP")
+        selection = int(input("\n"))
+        if(selection==1):
+            new_name = input("\nIngrese el nombre: ")
+            self.set_name(new_name)
+        elif(selection==2):
+            new_last_name = input("\nIngrese el apellido: ")
+            self.set_last_name(new_last_name)
+        elif(selection==3):
+            self.get_birth_date().modify_date()
+        elif(selection==4):
+            new_curp = input("\nIngrese CURP: ")
+            self.set_curp(new_curp)
+        else:
+            print("\nOpción no válida")
+        print(f"\nInformación guardada: {self.show_visitor}")
+    def calculate_ticket_cost(self):
+        if(self.is_an_adult()):
+            if(self.total_visits%5==0):
+                return 100*.08
+            else: return 100
+        else:
+            if(self.total_visits%5==0):
+                return 50*0.8
+            else: return 50
+    def is_an_adult(self):
+        actual_year = 2024
+        person_year = self.get_birth_date().get_year()
+        visitors_age = actual_year - person_year
+        return (visitors_age >= 18)
